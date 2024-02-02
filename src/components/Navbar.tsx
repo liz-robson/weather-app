61236a7933cda1efe0a87032e513b056d93ae4fb
Liz Robson <lizziearobson@gmail.com>
2024-02-02 12:01:42 +0000

    adding react icons - location and sun

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
 
 type Props = {}
 
@@ -8,9 +10,12 @@ export default function Navbar({}: Props) {
         <div className="h-[80px] w-full flex items-center justify-between px-3  max-w-7xl mx-auto">
         <p className="flex items-center justify-center gap-2">
             <h2 className="text-gray-500 text-3x1">Weather</h2>
+            <MdWbSunny className="text-3x1 mt-1 text-yellow-300" />
         </p>
+        <section className="felx gap-2 items-center">
+            <MdMyLocation />
+        </section>
     </div>
-    Navbar
     </nav>
   )
 }
