692abf61556458c30984a64b011cec317e82bfd4
Liz Robson <lizziearobson@gmail.com>
2024-02-02 14:29:01 +0000

    adding props to search box

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
 
-type Props = {}
+type Props = {
+    className?: string;
+    value: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+    onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+}
 
-export default function SearchBox({}: Props) {
+export default function SearchBox(props : Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
-        <input type="text" />
-        <button><IoSearch /></button>
+    <form onSubmit={props.onSubmit} className="flex relative items-center justify-center h-10">
+        <input onChange={props.onChange} type="text" placeholder="Search Location" value={props.value} className="border-gray-300 rounded-l-md focus:outline-none focus:border-500 h-full"/>  
+        <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full"><IoSearch /></button>
     </form>
   )
 }
