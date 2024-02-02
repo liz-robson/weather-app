36cdc6fc52fa6b32ab9d9d05d06fd456e4ba4e84
Liz Robson <lizziearobson@gmail.com>
2024-02-02 16:27:52 +0000

    adding cn typescript file

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
+import { cn } from "../utils/cn";
 
 type Props = {
     className?: string;
@@ -8,11 +9,22 @@ type Props = {
     onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
 }
 
+
 export default function SearchBox(props : Props) {
   return (
-    <form onSubmit={props.onSubmit} className="flex relative items-center justify-center h-10">
-        <input onChange={props.onChange} type="text" placeholder="Search Location" value={props.value} className="border-gray-300 rounded-l-md focus:outline-none focus:border-500 h-full"/>  
-        <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full"><IoSearch /></button>
+    <form 
+    onSubmit={props.onSubmit} 
+    className={cn("flex relative items-center justify-center h-10", props.className)}>
+        <input 
+        onChange={props.onChange} 
+        type="text" 
+        placeholder="Search Location" 
+        value={props.value} 
+        className="border-gray-300 rounded-l-md focus:outline-none focus:border-500 h-full"/>  
+        <button 
+        className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full">
+            <IoSearch />
+            </button>
     </form>
   )
 }
