ed73c9fbe3c91289bea06ccbd27c587b8dbcaa10
Liz Robson <lizziearobson@gmail.com>
2024-02-02 11:44:49 +0000

    additions to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function Navbar({}: Props) {
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-        <div className="h-[80px] flex items-center justify-between px-3 w-full max-w-7xl mx-auto">
+        <div className="h-[80px] w-full flex items-center justify-between px-3  max-w-7xl mx-auto">
         <p className="flex items-center justify-center gap-2">
             <h2 className="text-gray-500 text-3x1">Weather</h2>
         </p>
