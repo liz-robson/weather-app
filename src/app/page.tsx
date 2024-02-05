d4ae63af4ddc79b9ce2c9d4e34f44e97384b3f97
Liz Robson <lizziearobson@gmail.com>
2024-02-05 09:26:15 +0000

    setting up types for weather data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,68 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 
+// http://api.openweathermap.org/data/2.5/forecast?q=london,uk&appid=d3a7ef82fb67b38f0ad7c0437307e653&cnt=56
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherListItem[];
+  city: City;
+}
+
+interface WeatherListItem {
+  dt: number;
+  main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+  };
+  weather: Weather[];
+  clouds: {
+      all: number;
+  };
+  wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+      pod: string;
+  };
+  dt_txt: string;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface City {
+  id: number;
+  name: string;
+  coord: {
+      lat: number;
+      lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
+
 export default function Home() {
   return (
    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
