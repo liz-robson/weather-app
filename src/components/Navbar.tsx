6c8508aa530821115df1dd7c2e28ca1397c801f0
Liz Robson <lizziearobson@gmail.com>
2024-02-05 21:23:58 +0000

    handling search box functions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,6 +39,22 @@ async function handleInputChange(value :string) {
   }
 }
 
+function handleSuggestionClick(value: string) {
+  setCity(value);
+  setShowSuggestions(false);
+}
+
+function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault(); // stops page from refreshing
+if (suggestions.length === 0) {
+  setError("Location not found");
+} else {
+  setError("");
+  setShowSuggestions(false);
+  setCity("");
+ }
+}
+
 return (
   <>
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
@@ -54,8 +70,14 @@ return (
             <div> 
                 <SearchBox 
                 value={city}
-                // onSubmit={}
+                onSubmit={handleSubmitSearch}
                 onChange={(e)=> handleInputChange(e.target.value)} /> 
+                <SuggestionBox 
+                error={error}
+                handleSuggestionClick={handleSuggestionClick}
+                suggestions={suggestions}
+                showSuggestions={showSuggestions}
+                />
             </div>
         </section>
     </div>
@@ -74,8 +96,20 @@ function SuggestionBox({ suggestions, showSuggestions, handleSuggestionClick, er
 
 {
   return (
+    <>{((showSuggestions && (suggestions.length > 1) || error) && (
     <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
-    <li className="cursor-pointer p-1 rounded hover:bg-gray-200"></li>
+      {error && suggestions.length<1 && (
+      <li className="text-red-500 p-1">{error}</li>
+      )}
+      {suggestions.map((item, index) => (
+    <li 
+    key={index}
+    onClick={() => handleSuggestionClick(item)}
+    className="cursor-pointer p-1 rounded hover:bg-gray-200">
+      {item}
+    </li>
+      ))}
     </ul>
+  ))}</>
   )
 }
